c0d05eb1f36e602333264985d7dd5ba1680a298b
Ilya Posov <iposov@gmail.com>
2018-03-03 00:21:08 +0300

    working on lamps

--- tasks/lamps/model/Connection.js
+++ tasks/lamps/model/Connection.js
@@ -3,8 +3,9 @@ export class Connection {
     _terminal1;
     _terminal2;
     _resistance;
+    _emf;
 
-    constructor(terminal1, terminal2, resistance) {
+    constructor(terminal1, terminal2, resistance, emf) {
         this._terminal1 = terminal1;
         this._terminal2 = terminal2;
         this._resistance = resistance;
---
+++ tasks/lamps/model/eq_solver.js
@@ -0,0 +1,15 @@
+/*
+
+e - (n - 1)    loops
+n              points
+
+
+total e + 1 equations
+e  variables
+*/
+
+export class EqSolver {
+
+
+
+}
